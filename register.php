f13cb0f1018b6add1d020c8e2647b7a789014b1a
Beper Ong <beper.ong@benilde.edu.ph>
2017-01-17 17:33:26 +0800

    Dat offset tho

--- register.php
+++ register.php
@@ -8,7 +8,7 @@
 	<body>
 		<div class="container">
 			<h1 class="text-center">Student Registration</h1>
-			<div class="col-offset-lg-3 col-lg-6">
+			<div class="col-lg-offset-3 col-lg-6">
 					<div class="well">
 						<form method="GET" action="welcome.php"
 							class="form-horizontal">
