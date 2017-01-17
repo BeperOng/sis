d1c3979a73515ac523d97678fb299368ba1cbc08
Beper Ong <beper.ong@benilde.edu.ph>
2017-01-17 17:14:52 +0800

    Commit PHP with bootstrap

---
+++ register.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Student Registration </title>
+		<link href="css/bootstrap.min.css" rel="stylesheet" />
+	</head>
+
+	<body>
+		<div class="container">
+			<h1 class="text-center">Student Registration</h1>
+			<div class="col-offset-3 col=lg-6">
+					<div class="well">
+						<form method="GET" action="welcome.php"
+							class="form-horizontal">
+							<div class="form-group">
+								<label class="control-label">
+									Student ID
+								</label>
+								<input type="text" name="sid"
+									lass="form-horizontal" required />
+							</div>
+						</form>
+					</div>
+			</div>
+		</div>
+
+	</body>
+
+</html>
